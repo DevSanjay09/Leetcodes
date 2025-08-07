c794f01c010bf03d7c46075e9a592e4fd1d9d5af
sanjay <152505629+DevSanjay09@users.noreply.github.com>
2025-08-07 22:52:19 +0530

    Time: 6 ms (16.07%), Space: 46.6 MB (97.42%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,20 +1,42 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length - 1;
-        
-        while (left < right) {
-            int sum = numbers[left] + numbers[right];
-            
-            if (sum == target) {
-                return new int[]{left + 1, right + 1};
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            int complement = target - numbers[i];
+
+            if (map.containsKey(complement)) {
+                // +1 for 1-based indexing
+                return new int[]{map.get(complement) + 1, i + 1};
             }
+
+            // Store the current number and its index
+            map.put(numbers[i], i);
         }
-        
-        return new int[]{-1, -1}; 
+
+        // If no solution is found (guaranteed by problem there will be one)
+        return new int[]{-1, -1};
     }
 }
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         int left = 0;
+//         int right = numbers.length - 1;
+
+//         while (left < right) {
+//             int sum = numbers[left] + numbers[right];
+
+//             if (sum == target) {
+//                 return new int[]{left + 1, right + 1}; // 1-based indexing
+//             } else if (sum < target) {
+//                 left++; // Need a bigger number
+//             } else {
+//                 right--; // Need a smaller number
+//             }
+//         }
+
+//         return new int[]{-1, -1}; // Just in case, although the problem says one solution exists
+//     }
+// }
